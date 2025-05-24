d76e4439cce430b9663a52e42cfc5b06ed6d2bea
Kunut0 <maxcunmun@gmail.com>
2025-05-24 15:54:04 +0200

    changes yeah

--- gamejamulm/Script/player_1.gd
+++ gamejamulm/Script/player_1.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-@onready var sprite = $PlaceholderSprite
+@onready var sprite = $AnimatedSprite2D
 @onready var camera = $Camera2D
 @onready var dash_timer = $DashTimer
 @onready var dash_collision = $DashCollision
 @onready var default_collision = $NormalCollision
+@onready var lichtkegel: Area2D = $Lichtkegel
 
 signal flashlight
 
@@ -20,6 +21,16 @@ var dash_allowed = true
 var shadow_ref
 var respawn_ref
 
+var light_timer
+var lichtkegel_sichtbar: bool = false
+var enemy_sight: bool = false
+
+func _ready() -> void:
+	shadow_ref = get_tree().get_first_node_in_group("shadow")
+	
+	lichtkegel.hide()
+	lichtkegel.monitoring = false
+
 func _process(delta: float) -> void:
 	
 	#generates gravity for player
@@ -27,13 +38,19 @@ func _process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	#gets direction imput
-	direction =  Input.get_axis("ui_a", "ui_d")
+	direction = Input.get_axis("ui_a", "ui_d")
 	
 	#checks direction to flip sprite
 	if direction < 0:
-		sprite.flip_h = true
-	else:
-		sprite.flip_h = false
+		self.scale.y = -1
+		self.rotation_degrees = 180
+		sprite.play("walk")
+	elif direction > 0:
+		self.scale.y = 1
+		self.rotation_degrees = 0
+		sprite.play("walk")
+	elif direction == 0:
+		sprite.play("default")
 	
 	#generates character movement
 	if direction:
@@ -43,7 +60,7 @@ func _process(delta: float) -> void:
 			velocity.x = speed * direction
 	else:
 		if dashing:
-			velocity.x = dash_speed * 1
+			velocity.x = dash_speed * direction
 		else:
 			velocity.x = move_toward(velocity.x, 0, speed)
 	
@@ -62,12 +79,30 @@ func _process(delta: float) -> void:
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("ui_select"):
-		await get_tree().create_timer(0.5).timeout
-		flashlight.emit()
+		if lichtkegel_sichtbar == false:
+			lichtkegel.show()
+			lichtkegel.monitoring = true
+			lichtkegel_sichtbar = true
+			
+	
+	
+	elif Input.is_action_just_released("ui_select"):
+		if lichtkegel_sichtbar == true:
+			lichtkegel.hide()
+			lichtkegel.monitoring = false
+			lichtkegel_sichtbar = false
+			await get_tree().create_timer(0.5).timeout
+			flashlight.emit()
+	
+	if lichtkegel_sichtbar and enemy_sight:
+		light_timer += 1*delta
+		if light_timer > 1:
+			flashlight.emit()
+	else:
+		light_timer = 0
 	
 	#Kamera Lerping
-	shadow_ref = get_tree().get_first_node_in_group("shadow")
-	var pos_diff = self.global_position - shadow_ref.global_position
+	var pos_diff = global_position - shadow_ref.global_position
 	var camera_pos: float
 	if  pos_diff.x < -200:
 		camera_pos = 100
@@ -93,8 +128,11 @@ func _on_timer_timeout() -> void:
 
 func _on_lichtkegel_body_entered(body: Node2D) -> void:
 	if body.is_in_group("shadow"):
+		enemy_sight = true
 		flashlight.connect(body.stun)
+
 
 func _on_lichtkegel_body_exited(body: Node2D) -> void:
 	if body.is_in_group("shadow"):
+		enemy_sight = false
 		flashlight.disconnect(body.stun)
