0e6e928127ec43acb9419d453f9fc0f7c968157a
Kunut0 <maxcunmun@gmail.com>
2025-05-25 15:17:01 +0200

    timer

--- gamejamulm/Script/level_anxiety.gd
+++ gamejamulm/Script/level_anxiety.gd
@@ -4,4 +4,4 @@ var timer_szene = preload("res://Szene/global_timer.tscn")
 
 func _ready() -> void:
 	var timer = timer_szene.instantiate()
-	get_tree().root.add_child(timer)
+	get_tree().root.add_child.call_deferred(timer)
--- gamejamulm/Script/scenechange.gd
+++ gamejamulm/Script/scenechange.gd
@@ -3,5 +3,5 @@ extends Area2D
 @export var newScene: String
 
 func _on_body_entered(body: Node2D) -> void:
-	if body. is_in_group("player1"):
+	if body.is_in_group("player1"):
 		get_tree().call_deferred("change_scene_to_file", newScene)
