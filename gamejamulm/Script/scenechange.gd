b475809931f8430a7fa030a81628ec26761aeffc
Egzon Tola <eggitola@gmail.com>
2025-05-25 15:52:46 +0200

    Push 15:52

--- gamejamulm/Script/scenechange.gd
+++ gamejamulm/Script/scenechange.gd
@@ -3,5 +3,9 @@ extends Area2D
 @export var newScene: String
 
 func _on_body_entered(body: Node2D) -> void:
-	if body. is_in_group("player1"):
+	print(body)
+	if body.is_in_group("player1"):
+		print("dude")
+		get_tree().call_deferred("change_scene_to_file", newScene)
+	elif get_tree().current_scene.name == "tutorial_2" and body.is_in_group("shadow"):
 		get_tree().call_deferred("change_scene_to_file", newScene)
