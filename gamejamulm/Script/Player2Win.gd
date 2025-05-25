be142824ca5c44bb64968acdbf895bbe91eab4d1
fredddie-mrbs <freddiemarbs@gmail.com>
2025-05-25 17:19:51 +0200

    intro, tutorial und outro

--- gamejamulm/Script/Player2Win.gd
+++ gamejamulm/Script/Player2Win.gd
@@ -5,7 +5,7 @@ var t
 func _ready() -> void:
 	GlobalTimer.stop()
 	
-	$RichTextLabel.text = "[font_size=112][center]The Runner overcame his shadows and escaped the lab.[p]"
+	$RichTextLabel.text = "[font_size=112][center]The Shadows were able to prevent the Runner from escaping.[p]"
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Szene/Start.tscn")
